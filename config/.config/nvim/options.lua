73be9a8cc28869f04c64248e44648b0933ee6ba6
lukavyi <lukavyi@me.com>
2025-09-16 09:41:26 +0200

    add relativenumber = true to nvim

---
+++ config/.config/nvim/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- Enable relative line numbers (hybrid with absolute)
+vim.opt.relativenumber = true
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
