32c58073490fbadab40cc9df33b87fbf769ebe3e
lukavyi <lukavyi@me.com>
2025-09-27 16:49:38 +0200

    enable soft wrap and indentation for better text readability in nvim

--- nvchad-custom/options.lua
+++ nvchad-custom/options.lua
@@ -5,5 +5,10 @@ require "nvchad.options"
 -- Enable relative line numbers (hybrid with absolute)
 vim.opt.relativenumber = true
 
+-- Enable soft wrap (line wrap without inserting line breaks)
+vim.opt.wrap = true
+vim.opt.linebreak = true  -- Wrap at word boundaries
+vim.opt.breakindent = true  -- Preserve indentation in wrapped text
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
